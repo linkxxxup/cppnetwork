12a05ff4a0434b86db7464c2ef58b8d87d619761
linkxxxup <whut_zgy@163.com>
2023-07-11 02:54:23 -0700

    small change

--- src/rpc_client_main.cpp
+++ src/rpc_client_main.cpp
@@ -8,26 +8,6 @@ int close_log = 0;
 using namespace wut::zgy::cppnetwork;
 
 int main() {
-    // 获取日志配置
-//    Conf conf("../conf/log.conf");
-//    assert(conf.get_err_code() == 0);
-//    // 初始化日志
-//    Log *log = Log::get_instance();
-//    assert(log->init(conf) == 0);
-//    // 获取网络参数配置
-//    Conf conf_net("../conf/net_data.conf");
-//    CHECK_RET(conf_net.get_err_code()==0, "net_data.conf error");
-//    // 初始化网络参数
-//    auto net_data = NetData::get_instance();
-//    CHECK_RET(net_data->init(conf_net)==0, "net_data init error");
-//    // 获得服务套接字并初始化
-//    auto cli_fd = RpcClient::get_instance();
-//    CHECK_RET(cli_fd->init(net_data)==0, "client init error");
-//    cli_fd->run();
-//    std::string msg1 = cli_fd->call<void>("foo_1").error_msg();
-//    std::string msg2 = cli_fd->call<int>("foo_2", 10).error_msg();
-//    std::string msg3 = cli_fd->call<int>("foo_2", 10).error_msg();
-//    cli_fd->close();
     MyTask* task = MyTask::get_instance();
     task->run();
     auto ret1 = task->call<void>("foo_1");
